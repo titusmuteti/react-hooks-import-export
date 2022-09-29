4b51f9d02501be24103227cc3a466b913c5c6b42
Titus Mumo <titusmumo@gmail.com>
2022-09-30 00:19:39 +0300

    Completed assignment

--- src/ColoradoStateParks.js
+++ src/ColoradoStateParks.js
@@ -1,8 +1,22 @@
 import React from "react";
 import howManyParks from "./parks/howManyParks";
+import MesaVerde from "./parks/MesaVerde";
+import { wildlife, trees,elevation } from "./parks/RockyMountain";
 
 function ColoradoStateParks() {
   howManyParks(); // => "42 parks!"
 
-  return <h1>Colorado State Parks!</h1>;
-}
+  console.log(trees);
+  wildlife();
+  elevation()
+
+  return (
+    <>
+
+    <h1>Colorado State Parks!</h1>
+    <MesaVerde />
+    </>
+  
+)}
+
+export default ColoradoStateParks
